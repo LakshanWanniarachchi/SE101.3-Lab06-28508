6ce8d37dd39aa11958d730fbd7d926e6e921cfeb
Lakshan Lakshitha <134108423+LakshanWanniarachchi@users.noreply.github.com>
2023-06-15 10:59:38 +0530

    Practical - 05

---
+++ ShapeExe.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shapeexe;
+
+/**
+ *
+ * @author Lab User
+ */
+public class ShapeExe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+    Circle c1 = new Circle (7.00f);
+    c1.display();
+    
+    Rectangle r1 = new Rectangle (12.00f,5.00f);
+    r1.display();
+    }
+    
+}
